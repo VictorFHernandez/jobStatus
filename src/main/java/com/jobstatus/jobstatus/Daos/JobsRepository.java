b64e54ab634ef878588cc11fc12aaff691021787
VictorFHernandez <victorfhernandez3@gmail.com>
2021-08-20 14:46:55 -0500

    working on controller views

--- src/main/java/com/jobstatus/jobstatus/Daos/JobsRepository.java
+++ src/main/java/com/jobstatus/jobstatus/Daos/JobsRepository.java
@@ -1,7 +1,8 @@
 package com.jobstatus.jobstatus.Daos;
 
+import com.jobstatus.jobstatus.Models.Jobs;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobsRepository extends JpaRepository<JobsRepository, Long> {
-
+    Jobs getById(long id);
 }
--- src/main/java/com/jobstatus/jobstatus/MySQL/Migration.sql
+++ src/main/java/com/jobstatus/jobstatus/MySQL/Migration.sql
@@ -6,7 +6,7 @@ USE jobStatus_db;
 CREATE USER IF NOT EXISTS jobStatus_user@localhost IDENTIFIED BY 'codeup';
 GRANT ALL ON jobStatus_db.* TO jobStatus_user@localhost;
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS jobs (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     job_name VARCHAR (255) NOT NULL,
     status VARCHAR (255) NOT NULL,
