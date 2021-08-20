b9723465e7dd764798a7e542be8d2e70548b5c32
VictorFHernandez <victorfhernandez3@gmail.com>
2021-08-20 09:39:26 -0500

    created reposotorie

---
+++ src/main/java/com/jobstatus/jobstatus/JobStatusApplication.java
@@ -0,0 +1,13 @@
+package com.jobstatus.jobstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobStatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobStatusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobstatus/jobstatus/JobStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobstatus.jobstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobStatusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
